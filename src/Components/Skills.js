d5eb8f3e70246e994c0a52f395702c99dd2fb9f8
Abhijith Dasan <abhijithdasan05@gmail.com>
2023-12-22 23:51:55 +0530

    Skill-bar Modified

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -10,9 +10,82 @@ function Skills () {
             <div className='skill-box'>
                 <div className='skill-lbox'>
                     <div className='skill-box-head'>TECHNICAL SKILLS</div>
-                    <div>
-                        
+                    <div className = 'skill-bar'>
+                        <div className = 'skill-name'> 
+                            <p>HTML</p>
+                            <p>85%</p>
+                        </div>
+                        <div className = 'bar'>
+                            <div className ='obar'><div className = 'bar-1'></div>
+                            </div>
+                        </div>
                     </div>
+
+                    <div className = 'skill-bar'>
+                        <div className = 'skill-name'> 
+                            <p>CSS</p>
+                            <p>88%</p>
+                        </div>
+                        <div className = 'bar'>
+                            <div className ='obar'><div className = 'bar-2'></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'skill-bar'>
+                        <div className = 'skill-name'> 
+                            <p>JAVASCRIPT</p>
+                            <p>75%</p>
+                        </div>
+                        <div className = 'bar'>
+                            <div className ='obar'><div className = 'bar-3'></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'skill-bar'>
+                        <div className = 'skill-name'> 
+                            <p>REACT</p>
+                            <p>73%</p>
+                        </div>
+                        <div className = 'bar'>
+                            <div className ='obar'><div className = 'bar-4'></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'skill-bar'>
+                        <div className = 'skill-name'> 
+                            <p>BOOTSTRAP</p>
+                            <p>60%</p>
+                        </div>
+                        <div className = 'bar'>
+                            <div className ='obar'><div className = 'bar-5'></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className = 'skill-bar'>
+                        <div className = 'skill-name'> 
+                            <p>C/C++</p>
+                            <p>89%</p>
+                        </div>
+                        <div className = 'bar'>
+                            <div className ='obar'><div className = 'bar-6'></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className = 'skill-bar'>
+                        <div className = 'skill-name'> 
+                            <p>PYTHON</p>
+                            <p>84%</p>
+                        </div>
+                        <div className = 'bar'>
+                            <div className ='obar'><div className = 'bar-7'></div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
                 <div className='skill-rbox'>
                     <div className='skill-box-head'>PROFESSIONAL SKILLS</div>
@@ -21,7 +94,6 @@ function Skills () {
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
