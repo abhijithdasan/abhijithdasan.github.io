33e9fc046a0ce5c3b522b3bb5a5685f4e1141b20
Abhijith Dasan <abhijithdasan05@gmail.com>
2024-02-18 09:50:10 +0530

    Updated The Bar

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,182 +1,63 @@
-// import React, { useEffect } from 'react';
+import React from 'react';
 import '../App.css';
 import './Skills.css';
 
+const Skill = ({ name, percentage, isCircular }) => {
+  if (isCircular) {
+    return (
+      <div className='skill-bar'>
+        <div className='skill-name'>
+          <p>{name}</p>
+          <p>{percentage}%</p>
+        </div>
+        <div className='circular-bar'>
+          <div className='bar-inner-circle' style={{ transform: `rotate(${(percentage / 100) * 360}deg)` }}></div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className='skill-bar'>
+        <div className='skill-name'>
+          <p>{name}</p>
+          <p>{percentage}%</p>
+        </div>
+        <div className='bar'>
+          <div className='rectangular-bar' style={{ width: `${percentage}%` }}></div>
+        </div>
+      </div>
+    );
+  }
+};
+
 function Skills() {
-  
   return (
-    <div id ="skills" className='skills'>
+    <div id="skills" className='skills'>
       <div className='skills-head'>MY SKILLS</div>
       <div className='skill-box'>
-        <div className='skill-lbox'>
+        <div className='left-container'>
           <div className='skill-box-head'>TECHNICAL SKILLS</div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>CSS</p>
-              <p>88%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-2'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-          
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-
+          <Skill name="HTML / CSS" percentage={85} />
+          <Skill name="Javascript" percentage={78} />
+          <Skill name="React.js" percentage={75} />
+          <Skill name="Node.js" percentage={70} />
+          <Skill name="Express.js" percentage={70} />
+          <Skill name="C / C++" percentage={80} />
+          <Skill name="Python" percentage={75} />
+          <Skill name="Django" percentage={65} />
+          <Skill name="MySQL" percentage={70} />
+          <Skill name="DSA" percentage={65} />
+          <Skill name="Excel" percentage={60} />
         </div>
-
-        {/* <div className='skill-rbox'>
+        <div className='right-container'>
           <div className='skill-box-head'>PROFESSIONAL SKILLS</div>
-        </div> */}
-        <div className='skill-lbox'>
-          <div className='skill-box-head'>TECHNICAL SKILLS</div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>CSS</p>
-              <p>88%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-2'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-          
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div>
-
+          <Skill name="Team Work" percentage={90} isCircular />
+          <Skill name="Leadership" percentage={87} isCircular />
+          <Skill name="Creativity" percentage={88} isCircular />
+          <Skill name="Communication" percentage={85} isCircular />
+          <Skill name="Problem Solving" percentage={88} isCircular />
+          <Skill name="Project management" percentage={75} isCircular />
         </div>
-
-
-        {/* <div className='skill-bar'>
-            <div className='skill-name'>
-              <p>HTML</p>
-              <p>85%</p>
-            </div>
-            <div className='bar'>
-              <div className='obar'>
-                <div className='bar-1'></div>
-              </div>
-            </div>
-          </div> */}
-
-
       </div>
     </div>
   );
